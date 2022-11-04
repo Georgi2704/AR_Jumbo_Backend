a1c51d49fecae68c1b4dd3e59532c16875bff575
Georgi2704 <jrmanev@gmail.com>
2022-11-04 15:18:59 +0100

    updates

--- server.js
+++ server.js
@@ -13,95 +13,95 @@ const PORT = 8080;
 app.use(express.static("."));
 
 app.post('/NavigationInstructions', function(req, res) {
-	console.log("Navigation Instructions - Retrieval Request...")
-	var beaconName = req.body.beaconNodeScannedName;
-	var destination = req.body.destination;
-	emitter.once('navigationInstructionsRetrieved', function (snapshot){
-		if (snapshot.val() == undefined) {
-			console.log("Entry does not exist...");
-		}
-		// var json = formatter.formatJSON(snapshot);
-		res.send(snapshot.val());
-		console.log("Request Completed....\n\n");
-	});
-	db.getNavigationInstructions(destination);
+    console.log("Navigation Instructions - Retrieval Request...")
+    var beaconName = req.body.beaconNodeScannedName;
+    var destination = req.body.destination;
+    emitter.once('navigationInstructionsRetrieved', function (snapshot){
+        if (snapshot.val() == undefined) {
+            console.log("Entry does not exist...");
+        }
+        // var json = formatter.formatJSON(snapshot);
+        res.send(snapshot.val());
+        console.log("Request Completed....\n\n");
+    });
+    db.getNavigationInstructions(destination);
 });
 
 app.post('/DestinationList', function(req, res) {
-	console.log('Destination List - Retrieval Requested...');
-	emitter.once('destinationListRetrieved', function (snapshot){
-		if(snapshot.val() == undefined){
-			console.log('Error retrieving list...');
-			res.send("Error retrieving list...");
-		} else {
-			var arr = formatter.removeChildrenFromJSON(snapshot);
-			var json = JSON.stringify(arr)
-			res.send(json);
-		}
-		console.log("Request Completed....\n\n");
-	});
-	db.getDestinationList();
+    console.log('Destination List - Retrieval Requested...');
+    emitter.once('destinationListRetrieved', function (snapshot){
+        if(snapshot.val() == undefined){
+            console.log('Error retrieving list...');
+            res.send("Error retrieving list...");
+        } else {
+            var arr = formatter.removeChildrenFromJSON(snapshot);
+            var json = JSON.stringify(arr)
+            res.send(json);
+        }
+        console.log("Request Completed....\n\n");
+    });
+    db.getDestinationList();
 });
 
 //UploadCustomMap
 app.post('/UploadCustomMap', function(req, res) {
-	console.log('Upload Custom Map - Requested...');
-	var snapshot = req.body;
-	var uid = snapshot.uid;
-	var destination = snapshot.destination;
-	delete snapshot.uid;
+    console.log('Upload Custom Map - Requested...');
+    var snapshot = req.body;
+    // var uid = snapshot.uid;
+    var destination = snapshot.destination;
+    // delete snapshot.uid;
 
-	emitter.once("uploadCustomMapCompleted", function(bool){
-		if (bool) {
-			res.send("Upload Succeeded!");
-		} else {
-			res.send("Upload Failed!");
-		}
-		console.log("Request Completed...\n\n");
-	});
-	db.uploadCustomMap(uid, destination, snapshot);
+    emitter.once("uploadCustomMapCompleted", function(bool){
+        if (bool) {
+            res.send("Upload Succeeded!");
+        } else {
+            res.send("Upload Failed!");
+        }
+        console.log("Request Completed...\n\n");
+    });
+    db.uploadCustomMap(destination, snapshot);
 });
 
 //Request Map NAmes
 app.post('/DownloadCustomMapNames', function(req, res) {
-	console.log('Download Custom Map Names - Requested...');
-	var snapshot = req.body;
-	var uid = snapshot.uid;
+    console.log('Download Custom Map Names - Requested...');
+    var snapshot = req.body;
+    var uid = snapshot.uid;
 
-	emitter.once("downloadCustomMapNamesRetrieved", function(snapshot){
-		if(snapshot.val() == undefined){
-			console.log('Error retrieving list...');
-			res.send("Error retrieving list...");
-		} else {
-			//console.log(snapshot.val());
-			var arr = formatter.removeChildrenFromJSON(snapshot);
-			var json = JSON.stringify(arr)
-			res.send(json);
-		}
-		console.log("Request Completed....\n\n");
-	});
-	db.getCustomMapNames(uid);
+    emitter.once("downloadCustomMapNamesRetrieved", function(snapshot){
+        if(snapshot.val() == undefined){
+            console.log('Error retrieving list...');
+            res.send("Error retrieving list...");
+        } else {
+            //console.log(snapshot.val());
+            var arr = formatter.removeChildrenFromJSON(snapshot);
+            var json = JSON.stringify(arr)
+            res.send(json);
+        }
+        console.log("Request Completed....\n\n");
+    });
+    db.getCustomMapNames(uid);
 });
 
 //Download Custom Map
 app.post('/DownloadCustomMap', function(req, res) {
-	console.log('Download Custom Map - Requested...');
-	var snapshot = req.body;
-	var mapName = snapshot.map_name;
-	var uid = snapshot.uid;
+    console.log('Download Custom Map - Requested...');
+    var snapshot = req.body;
+    var mapName = snapshot.map_name;
+    var uid = snapshot.uid;
 
-	emitter.once("customMapRetrieved", function(snapshot){
-		if (snapshot.val() == undefined) {
-			console.log("Entry does not exist...");
-		}
-		// var json = formatter.formatJSON(snapshot);
-		res.send(snapshot.val());
-		console.log("Request Completed....\n\n");
-	});
-	db.getCustomMap(uid, mapName);
+    emitter.once("customMapRetrieved", function(snapshot){
+        if (snapshot.val() == undefined) {
+            console.log("Entry does not exist...");
+        }
+        // var json = formatter.formatJSON(snapshot);
+        res.send(snapshot.val());
+        console.log("Request Completed....\n\n");
+    });
+    db.getCustomMap(uid, mapName);
 });
 
 
 app.listen(PORT, function(){
-	console.log('Server Running on PORT:%s...\n', String(PORT));
+    console.log('Server Running on PORT:%s...\n', String(PORT));
 });
